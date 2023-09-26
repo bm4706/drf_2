1d2acac4fe4f3a90572cfca2ed2cf34103510546
bm4706 <bm4706@naver.com>
2023-09-26 16:49:37 +0900

    게시글에 댓글보기+ user값이 숫자인데 이를 email로 대체시키기

--- articles/serializers.py
+++ articles/serializers.py
@@ -2,23 +2,33 @@ from rest_framework import serializers
 
 from articles.models import Article, Comment
 
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()
+    
+    def get_user(self, obj):
+        return obj.user.email # 이러면 이제 user값이 email로 변경되어서 보여줌 
+    class Meta:
+        model = Comment
+        # fields = "__all__"
+        exclude = ("article",) # 댓글볼때 필요없는 부분 제거하는 곳
+        
+        
 class ArticleSerializer(serializers.ModelSerializer): # 모든 정보를 볼려고함
     user = serializers.SerializerMethodField()
+    comment_set = CommentSerializer(many=True) # 이 게시글의 댓글을 보기 위함
+    # 단 models.py 에서 article에 related_name을 설정해주면 그 설정한 값으로 바꿔줘야함 없으면 무조건 comment_set으로 해야한다.
+    likes = serializers.StringRelatedField(many=True)
     def get_user(self, obj):
         return obj.user.email
     class Meta:
         model = Article
-        fields = "__all__"
-
-class CommentSerializer(serializers.ModelSerializer):
-    
-    class Meta:
-        model = Comment
-        fields = "__all__"
-        
+        fields = "__all__"      
         
 class ArticleListSerializer(serializers.ModelSerializer): # 필요한 정보만 볼려고함
     user = serializers.SerializerMethodField()
+    
     def get_user(self, obj):
         return obj.user.email # 이러면 이제 user값이 email로 변경되어서 보여줌
     class Meta:
